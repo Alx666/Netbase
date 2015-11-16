5f4a219e290bc1864e27fe135a4585c4dedb1cbc
Alessio Greco <alxeyesoul@live.com>
2015-11-16 22:40:16 +0100

    1st state machine logic, need to test

--- Netbase.Shared/Session/SessionNonBlocking.cs
+++ Netbase.Shared/Session/SessionNonBlocking.cs
@@ -26,10 +26,12 @@ namespace Netbase.Shared
         private Packet m_hCurrent;
         private int m_iSendOffset;
 
-        private ISessionState m_hCurrentState;
-        private ISessionState m_hDisconnected;
-        private ISessionState m_hConnecting;
-        private ISessionState m_hActive;
+        private ISessionState                   m_hCurrentState;
+        private SessionStateDisconnected        m_hDisconnected;
+        private SessionStateConnecting          m_hConnecting;
+        private SessionStateJustConnected       m_hJustConnected;
+        private SessionStateActive              m_hActive;
+        private SessionStateJustDisconnected    m_hJustDisconnected;
 
 
 
@@ -40,87 +42,48 @@ namespace Netbase.Shared
 
         public SessionNonBlocking()
         {
-            m_hRecvBuffer   = new byte[1024];
-            m_hMs           = new MemoryStream(m_hRecvBuffer);
-            m_hReader       = new BinaryReader(m_hMs);
-            m_hToSend       = new Queue<Packet>();
+            m_hRecvBuffer       = new byte[1024];
+            m_hMs               = new MemoryStream(m_hRecvBuffer);
+            m_hReader           = new BinaryReader(m_hMs);
+            m_hToSend           = new Queue<Packet>();
+            m_hDisconnected     = new SessionStateDisconnected(this);
+            m_hConnecting       = new SessionStateConnecting(this);
+            m_hJustConnected    = new SessionStateJustConnected(this);
+            m_hActive           = new SessionStateActive(this);
+            m_hJustDisconnected = new SessionStateJustDisconnected(this);
 
-            m_hDisconnected = new SessionStateDisconnected(this);
-            m_hConnecting   = new SessionStateConnecting(this);
-            m_hActive       = new SessionStateActive(this);
-
-            m_hDisconnected.Next                          = m_hConnecting;
-            m_hConnecting.Next                            = m_hActive;
-            m_hActive.Next                                = m_hDisconnected;
-
-            m_hCurrentState                             = m_hDisconnected;
-        }
+            m_hDisconnected.Next            = m_hConnecting;
+            m_hJustConnected.Next           = m_hActive;
+            m_hActive.Next                  = m_hJustDisconnected;
+            m_hJustDisconnected.Next        = m_hDisconnected;
 
 
-        public void Update()
-        {
-            m_hCurrentState = m_hCurrentState.Update();
-
-            if (m_hSocket.Connected)
-            {
-                try
-                {
-                    SocketError eError = SocketError.Success;
-
-                    while (m_hToSend.Count > 0 && eError != SocketError.WouldBlock)
-                    {
-                        if(m_hCurrent == null)
-                        {
-                            m_hCurrent      = m_hToSend.Dequeue();
-                            m_iSendOffset   = 0;
-                        }
-
-                        int iTotal = m_hCurrent.DataSize + Packet.HeaderSize;
-                        m_iSendOffset += m_hSocket.Send(m_hCurrent.Buffer, m_iSendOffset, iTotal - m_iSendOffset, SocketFlags.None);
-
-                        if (m_iSendOffset == iTotal)
-                        {
-                            m_hCurrent = null;
-                            m_hCurrent.Recycle();
-                        }
-                    }
-
-                    byte bPacketId;
-                    while(ReceiveAll(this.m_hSocket, m_hRecvBuffer, out bPacketId) != SocketError.WouldBlock)
-                    {
-                        //Portiamo lo stream in posizione per leggere i dati
-                        m_hMs.Seek(Packet.DataSizeIndex, SeekOrigin.Begin);
-
-                        //Dobbiamo implementare il meccanismo per prendere il pacchetto giusto in base all'id ricevuto
-                        IAction hAction = Interpreter.Get(bPacketId);
-
-                        //Carichiamo la action
-                        hAction.LoadData(m_hReader);
-
-                        //Eseguiamo l'action                    
-                        hAction.Execute(this, this);
-                    }
-              
-                }
-                catch (Exception)
-                {
-                    if (Disconnected != null)
-                        Disconnected();
-                }
-            }     
+            m_hCurrentState      = m_hDisconnected;
         }
 
         public void Dispose()
         {
             m_hSocket.Shutdown(SocketShutdown.Both);
             m_hSocket.Close();
+            m_hSocket = null;
         }
 
+        public void Update()
+        {
+            try
+            {
+                m_hCurrentState = m_hCurrentState.Update();
+            }
+            catch (SocketException)
+            {
+                this.Dispose();
+                m_hCurrentState = m_hDisconnected;
+            }   
+        }
 
         public void Connect(string sAddr, int iPort)
         {
             m_hSocket           = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            m_hCurrentState     = m_hConnecting;
             m_hSocket.BeginConnect(sAddr, iPort, OnEndConnect, m_hSocket);            
         }
 
@@ -131,12 +94,12 @@ namespace Netbase.Shared
             {
                 m_hSocket.EndConnect(hRes);
                 m_hSocket.Blocking  = false;
-                m_hCurrentState     = m_hActive;
+                m_hCurrentState     = m_hJustConnected;
             }
             catch (Exception)
             {
-            }
-            
+                m_hCurrentState     = m_hJustDisconnected;
+            }            
         }
 
         //TODO: in modalità non di blocco, il metodo Send può risultare completato anche se invia una quantità di byte inferiore al numero di byte presente nel buffer.L'applicazione deve tenere traccia del numero di byte e ritentare l'operazione fino all'invio dei byte nel buffer
@@ -183,7 +146,6 @@ namespace Netbase.Shared
         private interface ISessionState
         {
             ISessionState Update();
-            ISessionState Next { get; set; }
         }
 
         private class SessionStateDisconnected : ISessionState
@@ -198,13 +160,15 @@ namespace Netbase.Shared
 
             public ISessionState Update()
             {
-                return this;
+                if (m_hOwner.m_hSocket != null)
+                    return Next;
+                else
+                    return this;
             }
         }
 
         private class SessionStateConnecting : ISessionState
         {
-            public ISessionState Next { get; set; }
             private SessionNonBlocking m_hOwner;
 
 
@@ -216,6 +180,46 @@ namespace Netbase.Shared
             public ISessionState Update()
             {
                 return this;
+            }
+        }
+
+        private class SessionStateJustConnected : ISessionState
+        {
+            public ISessionState Next { get; set; }
+
+            private SessionNonBlocking m_hOwner;
+
+            public SessionStateJustConnected(SessionNonBlocking hOwner)
+            {
+                m_hOwner = hOwner;
+            }
+
+            public ISessionState Update()
+            {
+                if (m_hOwner.Connected != null)
+                    m_hOwner.Connected();
+
+                return Next;
+            }
+        }
+
+        private class SessionStateJustDisconnected : ISessionState
+        {
+            public ISessionState Next { get; set; }
+
+            private SessionNonBlocking m_hOwner;
+
+            public SessionStateJustDisconnected(SessionNonBlocking hOwner)
+            {
+                m_hOwner = hOwner;
+            }
+
+            public ISessionState Update()
+            {
+                if (m_hOwner.Disconnected != null)
+                    m_hOwner.Disconnected();
+
+                return Next;
             }
         }
 
@@ -231,6 +235,42 @@ namespace Netbase.Shared
 
             public ISessionState Update()
             {
+                SocketError eError = SocketError.Success;
+
+                while (m_hOwner.m_hToSend.Count > 0 && eError != SocketError.WouldBlock)
+                {
+                    if (m_hOwner.m_hCurrent == null)
+                    {
+                        m_hOwner.m_hCurrent = m_hOwner.m_hToSend.Dequeue();
+                        m_hOwner.m_iSendOffset = 0;
+                    }
+
+                    int iTotal = m_hOwner.m_hCurrent.DataSize + Packet.HeaderSize;
+                    m_hOwner.m_iSendOffset += m_hOwner.m_hSocket.Send(m_hOwner.m_hCurrent.Buffer, m_hOwner.m_iSendOffset, iTotal - m_hOwner.m_iSendOffset, SocketFlags.None);
+
+                    if (m_hOwner.m_iSendOffset == iTotal)
+                    {
+                        m_hOwner.m_hCurrent = null;
+                        m_hOwner.m_hCurrent.Recycle();
+                    }
+                }
+
+                byte bPacketId;
+                while (ReceiveAll(m_hOwner.m_hSocket, m_hOwner.m_hRecvBuffer, out bPacketId) != SocketError.WouldBlock)
+                {
+                    //Portiamo lo stream in posizione per leggere i dati
+                    m_hOwner.m_hMs.Seek(Packet.DataSizeIndex, SeekOrigin.Begin);
+
+                    //Dobbiamo implementare il meccanismo per prendere il pacchetto giusto in base all'id ricevuto
+                    IAction hAction = Interpreter.Get(bPacketId);
+
+                    //Carichiamo la action
+                    hAction.LoadData(m_hOwner.m_hReader);
+
+                    //Eseguiamo l'action                    
+                    hAction.Execute(m_hOwner, m_hOwner);
+                }
+
                 return this;
             }
         }
